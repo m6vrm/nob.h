5692a887f13ad5efcf802953ea05586981ab6f5f
Roman Madyanov <romantaken@gmail.com>
2025-04-29 21:26:56 +0300

    Add example

---
+++ example/nob.c
@@ -0,0 +1,58 @@
+#define NOB_IMPLEMENTATION
+#include "../nob.h"
+
+#define BUILD_DIR "build/"
+
+const char* CFLAGS[] = {
+    "-Wall",       //
+    "-Wextra",     //
+    "-Wpedantic",  //
+    "-std=c99",    //
+    "-O2",         //
+    "-g",          //
+};
+
+const char* env(const char* name, const char* fallback) {
+    const char* env = getenv(name);
+    return env == NULL ? fallback : env;
+}
+
+const char* CC;
+
+int main(int argc, char* argv[]) {
+    rebuild_myself(argc, argv);
+
+    CC = env("CC", "cc");
+    Cmd cmd = {0};
+
+    log_print("build");
+    const char* src = "main.c";
+    const char* obj = BUILD_DIR "main.o";
+    fs_mkdir_recursive(obj);
+    cmd_append(&cmd, CC, "-c", src, "-o", obj);
+    arr_append_many(&cmd, CFLAGS, ARRAY_COUNT(CFLAGS));
+    compile_db_append(cmd);
+
+    // compile only if obj is outdated
+    if (fs_outdated(obj, src)) {
+        if (!proc_wait(cmd_exec_async(cmd)))
+            return EXIT_FAILURE;
+    }
+
+    log_print("write compilation db");
+    compile_db_write(BUILD_DIR "compile_commands.json");
+
+    log_print("link");
+    cmd.count = 0;  // reuse previous command memory
+#ifdef _WIN32
+    cmd_append(&cmd, CC, "-o", BUILD_DIR "example.exe", obj);
+#else   // _WIN32
+    cmd_append(&cmd, CC, "-o", BUILD_DIR "example", obj);
+#endif  // _WIN32
+    if (!proc_wait(cmd_exec_async(cmd)))
+        return EXIT_FAILURE;
+
+    log_print("success");
+
+    return EXIT_SUCCESS;
+}
